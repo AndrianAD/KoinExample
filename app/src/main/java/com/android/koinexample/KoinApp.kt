5d30fc4b1f0f01d5750ee6819dd5b7cfa79e5a4c
Tegritee <Tegritee@tutanota.com>
2019-09-12 12:28:07 +0300

    3.0

--- app/src/main/java/com/android/koinexample/KoinApp.kt
+++ app/src/main/java/com/android/koinexample/KoinApp.kt
@@ -8,15 +8,18 @@ import com.android.koinexample.wheel.Disk
 import com.android.koinexample.wheel.Steel
 import com.android.koinexample.wheel.Tire
 import org.koin.core.KoinComponent
+import org.koin.core.get
 import org.koin.core.inject
 
-class KoinApp: KoinComponent{
+class KoinApp : KoinComponent {
 
     val disk: Disk by inject()
     val tire: Tire by inject()
     val wheel: Wheel by inject()
 
-    val piston: Piston by inject()
+    val piston: Piston = get()
+    val piston2: Piston by inject()
+
     val cylinder: Cylinder by inject()
     val engine: Engine by inject()
 
--- app/src/main/java/com/android/koinexample/MainActivity.kt
+++ app/src/main/java/com/android/koinexample/MainActivity.kt
@@ -3,8 +3,6 @@ package com.android.koinexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.koinexample.badCar.BadCar
-import com.android.koinexample.badCar.BadEngine
-import com.android.koinexample.badCar.BadWheel
 import com.android.koinexample.car.Engine
 import com.android.koinexample.car.Wheel
 import com.android.koinexample.engine.Cylinder
@@ -12,9 +10,12 @@ import com.android.koinexample.engine.Piston
 import com.android.koinexample.wheel.Disk
 import com.android.koinexample.wheel.Steel
 import com.android.koinexample.wheel.Tire
+import org.koin.core.KoinComponent
 import org.koin.core.context.startKoin
+import org.koin.core.get
+import org.koin.core.inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), KoinComponent {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +23,28 @@ class MainActivity : AppCompatActivity() {
 
         startKoin { modules(firstModule) }
 
-        var car1 = Car(Engine(Piston(), Cylinder()), Wheel(Disk(Steel()), Tire()))
+
+        val badCar= BadCar()
+        badCar.makeSomeNoise()
+
+
+        val car1 = Car(Engine(Piston(), Cylinder()), Wheel(Disk(Steel()), Tire()))
         car1.makeSomeNoise()
 
-        var car3= BadCar()
-        car3.makeSomeNoise()
+        val car2: Car by inject()
+        car2.makeSomeNoise()
 
 
 
-
-
-
-
-//        var car2=KoinApp().car
+//        val car2=KoinApp().car
 //        car2.makeSomeNoise()
 //
-//        var car4=Car(KoinApp().engine,KoinApp().wheel)
-//        car4.makeSomeNoise()
-
-
+//
+//        val car: Car by inject()
+//        car.makeCar()
+//
+//        val car5:Car = get()
+//        car5.makeCar()
     }
 
 
